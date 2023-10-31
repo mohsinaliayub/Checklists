ecbb2bb5b66a55bd6a0c12807984a0e713ae4f3a
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2023-10-31 17:13:02 +0100

    navigate back to checklists view

--- Checklists/Controllers/AddItemViewController.swift
+++ Checklists/Controllers/AddItemViewController.swift
@@ -12,4 +12,13 @@ class AddItemViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    // MARK: - Actions
+    @IBAction func cancel() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func done() {
+        navigationController?.popViewController(animated: true)
+    }
 }
