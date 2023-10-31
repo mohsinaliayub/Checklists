d6a4431ba2d6ca8617fc7328866cb44136f64a78
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2023-10-31 12:13:17 +0100

    display dummy data

--- Checklists/Controllers/ChecklistViewController.swift
+++ Checklists/Controllers/ChecklistViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
+    
+    let todoItems = [
+        "Walk the dog", "Brush my teeth", "Learn iOS development",
+        "Soccer practice", "Eat ice cream"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // MARK: - Table View Data Source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        label.text = todoItems[indexPath.row]
+        
+        return cell
+    }
 }
 
