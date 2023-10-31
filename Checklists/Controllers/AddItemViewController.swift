d9111c965c86be080eaea4b58decef5939e121a3
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2023-10-31 17:09:17 +0100

    add controller file

---
+++ Checklists/Controllers/AddItemViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Mohsin Ali Ayub on 31.10.23.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
