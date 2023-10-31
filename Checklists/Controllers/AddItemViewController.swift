eb20e599609cc248d2165111654dc76ec83a5264
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2023-10-31 19:13:23 +0100

    read text from textfield

--- Checklists/Controllers/AddItemViewController.swift
+++ Checklists/Controllers/AddItemViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class AddItemViewController: UITableViewController {
     
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -19,6 +21,8 @@ class AddItemViewController: UITableViewController {
     }
     
     @IBAction func done() {
+        print("Contents of the text field: \(textField.text!)")
+        
         navigationController?.popViewController(animated: true)
     }
     
