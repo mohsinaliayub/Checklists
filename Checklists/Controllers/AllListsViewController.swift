ec2e8e1bc22ac060a84016bec439dd1e31914ee1
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2023-11-05 15:09:22 +0100

    add table view controller to show Checklists

---
+++ Checklists/Controllers/AllListsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AllListsViewController.swift
+//  Checklists
+//
+//  Created by Mohsin Ali Ayub on 05.11.23.
+//
+
+import UIKit
+
+class AllListsViewController: UITableViewController{
+    private let cellIdentifier = "ChecklistCell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+    }
+    
+    
+    // MARK: - Table View Data Source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.textLabel?.text = "List \(indexPath.row)"
+        return cell
+    }
+}
